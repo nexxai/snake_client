a878c5853a6f4b4f215f30fabe5c4ebd95777d11
JT Smith <nexxai@gmail.com>
2023-08-30 15:53:24 -0600

    Added onConnect event handler

--- client.js
+++ client.js
@@ -14,6 +14,11 @@ const connect = function () {
     console.log(message);
   });
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: JTS');
+  });
+
   return conn;
 };
 
