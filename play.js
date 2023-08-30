52298f2d4bf4111e1c36fe70acf29a5f1b8351f1
JT Smith <nexxai@gmail.com>
2023-08-30 16:06:10 -0600

    WASD keys are handled and sent to server

--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-connect();
-setupInput();
+let connection = connect();
+setupInput(connection);
