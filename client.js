d917eece1dfb48358fae68660141ae957300cc30
JT Smith <nexxai@gmail.com>
2023-08-30 16:08:00 -0600

    Updating initials

--- client.js
+++ client.js
@@ -16,7 +16,7 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    conn.write('Name: JTS');
+    conn.write('Name: NXI');
   });
 
   return conn;
