684281f7ef12bd4d54ff8a64859ee83a6cba22bf
JT Smith <nexxai@gmail.com>
2023-08-30 16:29:32 -0600

    Added constant movement ability

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
 const { ENCODING, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require('./constants');
 let connection;
+let direction;
+let moving;
 
 const setupInput = function (conn) {
   connection = conn;
@@ -15,16 +17,33 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   } else if (MOVE_UP_KEY.includes(key)) {
-    connection.write('Move: up');
+    // connection.write('Move: up');
+    constantMovement('up');
   } else if (MOVE_LEFT_KEY.includes(key)) {
-    connection.write('Move: left');
+    // connection.write('Move: left');
+    constantMovement('left');
   } else if (MOVE_DOWN_KEY.includes(key)) {
-    connection.write('Move: down');
+    // connection.write('Move: down');
+    constantMovement('down');
   } else if (MOVE_RIGHT_KEY.includes(key)) {
-    connection.write('Move: right');
+    // connection.write('Move: right');
+    constantMovement('right');
   } else if (key == 'n') {
     connection.write('Say: nexxai got this');
   }
 };
+
+const constantMovement = function (direction) {
+  if (moving) {
+    clearInterval(moving);
+  }
+  move(direction);
+}
+
+const move = function(direction) {
+  moving = setInterval(() => {
+    connection.write(`Move: ${direction}`)
+  }, 100);
+} 
 
 module.exports = { setupInput };
